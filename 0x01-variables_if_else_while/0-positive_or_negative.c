24d8546353af6269d4c6799b480161567303e210
aleja115 <maira_lejandra@hotmail.com>
2020-09-17 17:48:50 +0000

    change if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - Prints if the number is negative or positive
-*Return: 0 
+* main - Prints if the number is negative or positive
+* Return: 0 
 */
 int main(void)
 {
