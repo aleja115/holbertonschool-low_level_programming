0306bc406cea15f73fbc7777330efc2252b51544
aleja115 <maira_lejandra@hotmail.com>
2020-09-19 02:27:47 +0000

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+char c = i;
+for (i = 97; i < 123; i++)
+if (i != 101 && i != 113)
+putchar(c);
+putchar('\n');
+return (0);
+}
+
