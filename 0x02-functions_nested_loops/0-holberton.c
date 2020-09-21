02acb8772bfd20d78b36721aad3259894c198b43
aleja115 <maira_lejandra@hotmail.com>
2020-09-21 17:41:52 +0000

    loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main
+ *
+ *Return: 0
+ **/
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar(10);
+return (0);
+}
